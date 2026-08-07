5471602e62f5d86233e79f0bf08eae347fb5f926
Pranshu Thakkar <thakkar.pranshu2@gmail.com>
2026-08-07 14:50:19 +0530

    Update: three-integer-sum - submission-0

--- Data Structures & Algorithms/three-integer-sum/submission-0.java
+++ Data Structures & Algorithms/three-integer-sum/submission-0.java
@@ -1,24 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list1 = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
+        List<List<Integer>> list = new ArrayList<>(); 
+        int i=0;
+        Arrays.sort(nums);
+        while(i<nums.length-2){
+            int l=i+1;
+            int r = nums.length-1;
+            while(l<r){
                 int sum = 0;
-                for(int k=j+1;k<nums.length;k++){
-                    sum  = nums[i]+nums[j]+nums[k];
-                    if(sum==0){
-                        ArrayList<Integer> list = new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        Collections.sort(list);
-                        if(!list1.contains(list)){
-                            list1.add(list);
-                        }
+                sum = nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    List<Integer> list1 = new ArrayList<>();
+                    list1.add(nums[i]);
+                    list1.add(nums[l]);
+                    list1.add(nums[r]);
+                    if(!list.contains(list1)){
+                        list.add(list1);
                     }
+                    l++;
+                    r--;
+                }
+                else if(sum<0){
+                    l++;
+                }
+                else if(sum>0){
+                    r--;
                 }
             }
+            i++;
         }
-        return list1;
+        return list;
     }
 }
