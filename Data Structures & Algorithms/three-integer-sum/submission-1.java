bbc0864b0f0a31b3153e79323ab5d6b3f5db866f
Pranshu Thakkar <thakkar.pranshu2@gmail.com>
2026-08-07 14:52:31 +0530

    Update: three-integer-sum - submission-1

--- Data Structures & Algorithms/three-integer-sum/submission-1.java
+++ Data Structures & Algorithms/three-integer-sum/submission-1.java
@@ -1,43 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list1 = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>(); 
+        int i=0;
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-2;i++){
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            int l = i+1;
-            int r = nums.length - 1;
+        while(i<nums.length-2){
+            int l=i+1;
+            int r = nums.length-1;
             while(l<r){
-                int sum = nums[i]+nums[l]+nums[r];
+                int sum = 0;
+                sum = nums[i]+nums[l]+nums[r];
                 if(sum==0){
-                    ArrayList<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[l]);
-                    list.add(nums[r]);
-                    Collections.sort(list);
-                    if(!list1.contains(list)){
-                        list1.add(list);
-
-                        l++;
-                        r--;
+                    List<Integer> list1 = new ArrayList<>();
+                    list1.add(nums[i]);
+                    list1.add(nums[l]);
+                    list1.add(nums[r]);
+                    if(!list.contains(list1)){
+                        list.add(list1);
                     }
-                    while(l < r && nums[l] == nums[l - 1]){
-                        l++;
-                    }  
-                    while(l < r && nums[r] == nums[r + 1]){
-                        r--;
-                    }
-                }
-                else if(sum>0){
+                    l++;
                     r--;
                 }
                 else if(sum<0){
                     l++;
                 }
-
+                else if(sum>0){
+                    r--;
+                }
             }
+            i++;
         }
-        return list1;
+        return list;
     }
 }
