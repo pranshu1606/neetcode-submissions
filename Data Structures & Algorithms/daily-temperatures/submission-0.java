59c854ebd19fda2ffeb7e1f9c8f6852a8a851d8c
Pranshu Thakkar <thakkar.pranshu2@gmail.com>
2026-08-13 22:04:07 +0530

    Update: daily-temperatures - submission-0

--- Data Structures & Algorithms/daily-temperatures/submission-0.java
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
-        int[] arr  =  new int[temperatures.length];
+        int[] arr = new int[n];
         for(int i=0;i<n;i++){
-            for (int j=i+1;j<n;j++){
+            for(int j=i+1;j<n;j++){
                 if(temperatures[j]>temperatures[i]){
                     arr[i] = j-i;
                     break;
