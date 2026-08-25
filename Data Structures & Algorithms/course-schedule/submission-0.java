f2752c54c8e6a6b0f541163e2d897c0fb9835fa1
Pranshu Thakkar <thakkar.pranshu2@gmail.com>
2026-08-25 22:46:34 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<prerequisites.length;i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj.get(v).add(u);
+        }
+        int[] indegree = new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            for(int it:adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        List<Integer> topo = new ArrayList<>();
+        while(!q.isEmpty()){
+            int node = q.remove();
+            topo.add(node);
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.add(it);
+                }
+            }
+        }
+        if(topo.size()==numCourses){
+            return true;
+        }
+        return false;
+    }
+}
